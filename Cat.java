ec3c27fb2fe2927d13c8d5ecfca1171b4003b2cb
Nathan <19powelln@flintridgeprep.org>
2018-11-16 11:32:16 -0800

    Finished the Cat class, not completely sure if I'm doing the TestCatList class correctly (kinda out of order, but same result?)

--- Cat.java
+++ Cat.java
@@ -1,3 +1,25 @@
 
 public class Cat {
+    private String name;
+    private double weight;
+    void setName (String nName){
+        name = nName;
+    }
+    void setWeight (double nName){
+        weight = nName;
+    }
+    String getName (){
+        return name;
+    }
+    double getWeight(){
+        return weight;
+    }
+    public Cat(){
+    }
+    public Cat(String nName){
+        name = nName;
+    }
+    void meow(){
+        System.out.println("Meow!");
+    }
 }
---
+++ TestCatList.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class TestCatList{
+    void main(){
+        ArrayList<Cat> CatList=new ArrayList<Cat>();
+        /*for (int i=0; i<=4;i++){
+            CatList.add(i, Cat.i);
+        } */
+        Cat Steven= new Cat("Steven");
+        Cat Ian=new Cat("Ian");
+        Cat Brian=new Cat("Brian");
+        Cat George=new Cat("George");
+        CatList.add(0, Steven);
+        CatList.add(1, Ian);
+        CatList.add(2, Brian);
+        CatList.add(3, George);
+        //for (){
+        //}
+        
+    }
+}
