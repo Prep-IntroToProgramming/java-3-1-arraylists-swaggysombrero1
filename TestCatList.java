2ebb002368a80e73574d2e5101fb9c5169c00691
Nathan <19powelln@flintridgeprep.org>
2018-11-17 12:34:54 -0800

    Finished step 2, though I'm still not sure that I did it completely properly

--- TestCatList.java
+++ TestCatList.java
@@ -1,10 +1,7 @@
 import java.util.*;
 public class TestCatList{
-    void main(){
+    public static void main(){
         ArrayList<Cat> CatList=new ArrayList<Cat>();
-        /*for (int i=0; i<=4;i++){
-            CatList.add(i, Cat.i);
-        } */
         Cat Steven= new Cat("Steven");
         Cat Ian=new Cat("Ian");
         Cat Brian=new Cat("Brian");
@@ -13,8 +10,18 @@ public class TestCatList{
         CatList.add(1, Ian);
         CatList.add(2, Brian);
         CatList.add(3, George);
-        //for (){
-        //}
-        
+        int x=0;
+        for (Cat i: CatList){
+            double w = (Math.random()*10)+5;
+            CatList.get(x).setWeight(w);
+            x=x+1;
+        }
+        x=0;
+        for (Cat i:CatList){ 
+            System.out.println(CatList.get(x).getName() + " " + CatList.get(x).getWeight());
+            System.out.print(CatList.get(x).getName() + " says " );
+            CatList.get(x).meow(); 
+            x=x+1;
+        }
     }
 }
