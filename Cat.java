e223ea2bd42b981051144ec5aa2069ef7df41657
Nathan <19powelln@flintridgeprep.org>
2018-11-19 17:07:10 -0800

    Worked on two of the three bonuses

--- Cat.java
+++ Cat.java
@@ -4,21 +4,40 @@ public class Cat {
     void setName (String nName){
         name = nName;
     }
+
     void setWeight (double nName){
         weight = nName;
     }
+
     String getName (){
         return name;
     }
+
     double getWeight(){
         return weight;
     }
-    public Cat(){
+
+    public Cat(){ //overloaded Cat constructor
     }
+
     public Cat(String nName){
         name = nName;
     }
+
     void meow(){
-        System.out.println("Meow!");
+        double x = Math.random(); //created for the Bonus
+        if (weight>=10){ //created for last Bonus: larger cats have larger letters
+            if (x>=.25){
+                System.out.println("MEOW!"); 
+            } else {
+                System.out.println("Ruff?"); 
+            }
+        } else { 
+            if (x>=.25){
+                System.out.println("meow.");
+            } else {
+                System.out.println("ruff?");
+            }
+        } //.25 chance that the cat says some type of "ruff"
     }
 }
--- TestCatList.java
+++ TestCatList.java
@@ -2,25 +2,25 @@ import java.util.*;
 public class TestCatList{
     public static void main(){
         ArrayList<Cat> CatList=new ArrayList<Cat>();
-        Cat Steven= new Cat("Steven");
-        Cat Ian=new Cat("Ian");
+        Cat Steven= new Cat("Steven"); //instead of using the set method, I
+        Cat Ian=new Cat("Ian"); //used the constructor setter. Is that ok?
         Cat Brian=new Cat("Brian");
         Cat George=new Cat("George");
-        CatList.add(0, Steven);
-        CatList.add(1, Ian);
-        CatList.add(2, Brian);
+        CatList.add(0, Steven); //Have to create the Cat objects first, 
+        CatList.add(1, Ian); //couldn't use a for loop because of the different
+        CatList.add(2, Brian); //string names for the Cat constructors
         CatList.add(3, George);
-        int x=0;
+        int x=0; 
         for (Cat i: CatList){
             double w = (Math.random()*10)+5;
-            CatList.get(x).setWeight(w);
+            CatList.get(x).setWeight(w); //get(x) must refer to an int
             x=x+1;
         }
-        x=0;
+        x=0; //have to make x=0 again between methods to start from the top
         for (Cat i:CatList){ 
             System.out.println(CatList.get(x).getName() + " " + CatList.get(x).getWeight());
             System.out.print(CatList.get(x).getName() + " says " );
-            CatList.get(x).meow(); 
+            CatList.get(x).meow(); //don't want to say a void method
             x=x+1;
         }
     }
